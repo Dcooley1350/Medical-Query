f881c44dab2adfff5b4248a974c1df457e985493
Devin Cooley <dcooley1350@gmail.com>
2019-09-13 10:41:50 -0700

    Write html and script to display items searched for and number of results

--- src/main.js
+++ src/main.js
@@ -7,14 +7,18 @@ import { MedicalQuery } from './script.js';
 
 $(document).ready(function() {
   //Form submission
-  $('medical-query').submit(function(event) {
+  $('#medical-query').submit(function(event) {
     event.preventDefault();
     //Collect user input and store in variables
     let keyword = $("#keyword-input").val();
     let name = $("#name-input").val();
-    //Clear user input fields after storing user input
+
+    //Clear user input fields and return data after storing and displaying
     $("#keyword-input").text("");
     $("#name-input").text("");
+    $("#num-results").text("");
+    $("#keyword-results").text("");
+    $("#name-results").text("");
 
     //run api query and store results
     let medicalQuery = new MedicalQuery();
@@ -23,6 +27,10 @@ $(document).ready(function() {
     promise.then(function(response) {
       let body = JSON.parse(response);
       console.log(body);
+      $("#num-results").text();
+      $("#keyword-results").text(keyword);
+      $("#name-results").text(name);
+      $("#results").show();
     }, function(error) {
       $('#show-errors').text(`There was an error processing your request: ${error.message}`);
     });
--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ export class MedicalQuery {
   getDoctor(name,keyword) {
     return new Promise(function(resolve,reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${exports.apikey}&query=${keyword}&name=${name}&limit=100`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${exports.apikey}&query=${keyword}&limit=100`;
       request.onload =function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -18,3 +18,4 @@ export class MedicalQuery {
 
 
 // for loop can run to this.meta.total
+//&name=${name}
