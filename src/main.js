efc0753f8ff2f1b9072297aa4635426339bde93b
Devin Cooley <dcooley1350@gmail.com>
2019-09-13 09:37:50 -0700

    Write form fields in index.html and capture and store input with jquery

--- src/main.js
+++ src/main.js
@@ -6,7 +6,15 @@ import './styles.css';
 import { MedicalQuery } from './script.js';
 
 $(document).ready(function() {
+  //Form submission
   $('medical-query').submit(function(event) {
       event.preventDefault();
+      //Collect user input and store in variables
+      let keyword = $("#keyword-input").val();
+      let name = $("#name-input").val();
+      //Clear user input fields after storing user input
+      $("#keyword-input").text("");
+      $("#name-input").text("");
+
   });
 });
